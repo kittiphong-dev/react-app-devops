db9283b7c15df4e91a19351b5271a7ada5c81acd
kittiphong <kittiphong57412003@gmail.com>
2026-04-03 13:12:59 +0700

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+   <div>
+    <h1>Home</h1>
+    <p>Welcome to my app</p>
+    <p>This app is deployed using Jenkins</p>
+    <p>Version 0.0.2</p>
+    </div>
+  );
+}
